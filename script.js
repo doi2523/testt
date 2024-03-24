a023f5032cb280b8c1e8abd5082ca5d8d6582b9c
doibncm2003 <doibncm2003@gmail.com>
2024-03-24 21:21:25 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('nav');
+
+    menuToggle.addEventListener('click', function () {
+        nav.classList.toggle('active');
+        menuToggle.classList.toggle('open');
+    });
+});
